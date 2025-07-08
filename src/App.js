eb2526beefdc72920758e82dd75b660af8af2daf
Samuel <chintusamuel123@gmail.com>
2025-07-09 00:57:09 +0530

    Updated welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Samuel! My First React Website is Live!
         </p>
         <a
           className="App-link"
